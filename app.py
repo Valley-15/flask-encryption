96a6d90d43b5212a75c8c752ff05ee60d7e7d559
Adnaan <adnaan.2735@gmail.com>
2025-03-21 18:56:53 +0530

    making it ready for deployment

--- app.py
+++ app.py
@@ -71,5 +71,6 @@ def decrypt():
 
     return jsonify({'decrypted': decrypted_text})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
